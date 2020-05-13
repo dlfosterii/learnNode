d4ecff0b84d4671ef317059c97f1e20e910df0e2
David Foster <dlfosterii@yahoo.com>
2020-05-12 23:40:06 -0400

    exercies 5 complete

--- filtered-ls.js
+++ filtered-ls.js
@@ -1,5 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
-const filePath = process.argv[2]
-const ext = process.argv[3]
+const filePath = process.argv[2];
+const ext = process.argv[3];
+
+fs.readdir(filePath, (err, list) => {
+    list.forEach((file) => {
+        if ('.' + ext === path.extname(file)){
+            console.log(file);
+        }
+    })
+})
+
